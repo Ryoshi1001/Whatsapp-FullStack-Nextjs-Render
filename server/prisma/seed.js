51a0a37f9d5fb8331b8d00b28063e027a8a9cc0f
ryoshi1001 <ryoshi16421@proton.me>
2024-10-21 06:38:17 -0700

    removed vercel.json file for Render

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 async function main() {
     await prisma.user.create({
         data: {
-            email: 'test@example.com',
+            email: 'testextra@example.com',
             name: 'Test User',
             profilePicture: '',
             about: 'This is a test user',
