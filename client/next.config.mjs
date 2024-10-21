995ac6917923a68f7ffcefe8460ef5756a8bcdab
ryoshi1001 <ryoshi16421@proton.me>
2024-10-21 09:06:24 -0700

    updated next.config.mjs file to try and fix image/drawings text messages

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-   domains: ["localhost"],
+   domains: [
+    "https://whatsappbackend-uqtf.onrender.com", 
+    "localhost"],
   }
  };
  
